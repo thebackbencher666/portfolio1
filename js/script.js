47bbf4262bf2c649c9cd227817860cd932148c1f
arif rabbani_49 <107574647+thebackbencher666@users.noreply.github.com>
2024-02-26 11:03:31 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*========================typing animation =========================*/
+var typed = new Typed(".typing", {
+    strings: ["", "Student", "Android Developer", "Web Developer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
